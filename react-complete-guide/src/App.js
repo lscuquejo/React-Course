a84a335211848309756ba4e730bd03965734c5da
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-12-04 14:53:27 +0000

    ep 25 - completed

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React APP</h1>
         <p>This is really working!</p>
-        <Person />
-          <Person />
-          <Person />
+        <Person name="Max" age="30"/>
+        <Person name="Leo" age="20">My Hobbies: Racing</Person>
+        <Person name="Lari" age="17"/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'Does that work now?'));
